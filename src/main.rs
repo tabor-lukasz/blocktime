e7cd44360baff6bbce39c8db361fb3102e8a4d51
lt <tabor.lukasz@gmail.com>
2021-05-18 00:47:41 +0200

    Warp instead of raw socket

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,23 @@
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
 use std::sync::Arc;
+use warp::{Filter, Rejection, Reply};
 
 mod eth_client;
-mod responses;
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("0.0.0.0:12345").unwrap();
-
     let client = Arc::new(eth_client::EthClient::new().await);
+    
+    let route = warp::path!("currentBlockTime").and_then( move || {
+        handle_connection(client.clone())
+    });
+    let routes = route.with(warp::cors().allow_any_origin());
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        let client_cpy = client.clone();
-
-        tokio::spawn(async move { handle_connection(stream, client_cpy).await });
-    }
+    warp::serve(routes).run(([0, 0, 0, 0], 12345)).await;
 }
 
-async fn handle_connection(mut stream: TcpStream, client: Arc<eth_client::EthClient>) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-
-    let get = b"GET /currentBlockTime HTTP/1.1\r\n";
-
-    let response = if buffer.starts_with(get) {
-        match client.get_eth_ts().await {
-            Some(v) => responses::get_block_time(&v),
-            None => responses::get_block_time("Unknown"),
+async fn handle_connection(client: Arc<eth_client::EthClient>) -> std::result::Result<impl Reply, Rejection> {
+    match client.get_eth_ts().await {
+            Some(v) => Ok(v),
+            None => Ok("Unknown".to_string())
         }
-    } else {
-        responses::get_not_found()
-    };
-
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
 }
